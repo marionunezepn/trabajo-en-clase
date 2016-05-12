e156ca5fb5313d26978d1c803ed6687b52f6d309
Mario Jose Nuñez Villacis <pepeseya@hotmail.com>
2016-05-12 08:23:52 -0500

    listo
    practica 1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,5 +1,2 @@
-frases=str(input('ingrese frase'))
-frase=frases.split(" ")
-for i in range(1,len(frase)+1):
-	palabra=len(frase[i].split(" "))
-	print ("frase",i,":",palabra)
+cadena = str(input("Escriba una frase: "))
+print("Numero de palabras: " + str(len(cadena.split(" "))))
