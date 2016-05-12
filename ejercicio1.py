cfc2def595889592d8609389b50493bbed9bb6d1
Mario Jose Nuñez Villacis <pepeseya@hotmail.com>
2016-05-12 08:21:41 -0500

    revasa el codigo
    no cuenta el numero de palabras

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,5 @@
-frase=str('ingrese farse')
+frases=str(input('ingrese frase'))
+frase=frases.split(" ")
+for i in range(1,len(frase)+1):
+	palabra=len(frase[i].split(" "))
+	print ("frase",i,":",palabra)
