54a84b3fbbe05bf97fb43b7262da3200fe398fb2
oscarepv <oscar_e1993@hotmail.com>
2016-05-12 08:24:37 -0500

    Ejer
    mejorar impresion

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,8 @@
-frase=str('ingrese farse')
+frase=str(input('ingrese farse: '))
+palabra=1
+for i in range (len(frase)):
+	if (frase[i]==' '):
+		palabra=palabra+1
+
+print ('Palabras contadas: 'palabra)
+
