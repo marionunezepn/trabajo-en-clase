64842edd9f8ecad1e6841cbb63f2ae7047590415
oscarepv <oscar_e1993@hotmail.com>
2016-05-12 08:40:00 -0500

    Código Modificado
    modificado + archivo txt

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,14 +1,9 @@
-
-import os
-
-	def cargararchivo(self):
-		archivo = open("ag","r")
-		linea = archivo.readline()
-		if linea:
-			while linea:
-				if linea[-1] == '\n':
-					linea = linea[:-1] 
-				self.lista.append(linea)
-				linea = archivo.readline()
-		archivo.close()
+def leertxt():
+    archi=open('leer.txt','r')
+    linea=archi.readline()
+    while linea!="":
+        print (linea)
+        linea=archi.readline()
+    archi.close()
+leertxt()
 		
